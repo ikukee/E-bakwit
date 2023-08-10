2408d897898668fe89c005ade046a9e4fc2ff450
Paul Vincent E. Llesol <pvllesol@gbox.adnu.edu.ph>
2023-08-10 09:23:47 +0800

    Initialized dashboard and navbar

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,7 @@
+class MainController < ApplicationController
+    def index
+    end
+
+    def detailed_view
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to:"main#index"
+  get "/detailed_view", to: "main#detailed_view"
 end
