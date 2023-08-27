a31a5d122b09b6e5e2a2d7291c7a669b827c0c9b
James Mathew Juntado <jmjuntado@gbox.adnu.edu.ph>
2023-08-27 18:54:23 +0800

    added requestDate and edited main dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_27_100005) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_103212) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_27_100005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "cnum"
+    t.date "request_date"
   end
 
   create_table "users", force: :cascade do |t|
