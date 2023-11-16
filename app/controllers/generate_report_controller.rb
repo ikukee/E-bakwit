a4509a8bb7967b24aa19e73e6270beef04622f4b
ikukee <rmpanis2@gmail.com>
2023-11-16 18:21:52 +0800

    report generation per DISASTER

--- app/controllers/generate_report_controller.rb
+++ app/controllers/generate_report_controller.rb
@@ -1,11 +1,14 @@
 class GenerateReportController < ApplicationController
     
-    def generate 
+    def generate ## evac_centers/1/:disaster/generate
         @evac_center = EvacCenter.find(params[:evac_center])
         @disaster = Disaster.find(params[:disaster_id])
+        reliefGoods = ReliefGood.where("evac_center = ? AND disaster = ?").all
         result = CSV.generate do |csv|
+            ## EVACUEE
+            csv << ["GENERATED AS OF", Time.current]
             csv << ["DISASTER NAME", @disaster.name, "DISASTER TYPE",@disaster.disaster_type,"DATE OF OCCURENCE", @disaster.date_of_occurence]
-            csv << ["EVACUATION CENTER", @evac_center.name, "ADDRESS",@evac_center.barangay, "GENERATED AS OF", DateTime.now]
+            csv << ["EVACUATION CENTER", @evac_center.name, "ADDRESS",@evac_center.barangay]
             csv << ["Age Range", "No. Of MALEs", "No. Of FEMALEs", "TOTAL"]
             csv << ["INFANT", helpers.getInfants(@evac_center.id, @disaster.id,"Male"),helpers.getInfants(@evac_center.id, @disaster.id,"Female"),helpers.getInfants(@evac_center.id, @disaster.id,"Male") + helpers.getInfants(@evac_center.id, @disaster.id,"Female")]
             csv << ["TODDLER", helpers.getToddlers(@evac_center.id, @disaster.id,"Male"), helpers.getToddlers(@evac_center.id, @disaster.id,"Female"),helpers.getToddlers(@evac_center.id, @disaster.id,"Male") + helpers.getToddlers(@evac_center.id, @disaster.id,"Female")]
@@ -15,15 +18,20 @@ class GenerateReportController < ApplicationController
             csv << ["ADULTS",helpers.getAdults(@evac_center.id, @disaster.id,"Male"),helpers.getAdults(@evac_center.id, @disaster.id,"Female"),helpers.getAdults(@evac_center.id, @disaster.id,"Male") + helpers.getAdults(@evac_center.id, @disaster.id,"Female")]
             csv << ["SENIORS",helpers.getSeniors(@evac_center.id, @disaster.id,"Male"),helpers.getSeniors(@evac_center.id, @disaster.id,"Female"),helpers.getSeniors(@evac_center.id, @disaster.id,"Male") + helpers.getSeniors(@evac_center.id, @disaster.id,"Female")]
             csv << ["TOTAL",helpers.countGenderEvacuated(@evac_center.id,@disaster, "Male"),helpers.countGenderEvacuated(@evac_center.id,@disaster, "Female"),helpers.countGenderEvacuated(@evac_center.id,@disaster, "Male") + helpers.countGenderEvacuated(@evac_center.id,@disaster, "Female")]
+            ##RELIEF
+            ## csv << ["Name", "Unit of Measurement", "Quantity", "Cost"]
+            #reliefGoods.each do |relief|
+                #csv << [relief.name, relief.unit, relief.quantity, relief.cost]
+           # end
             
         end
         respond_to do |format|
             format.html
-            format.xls {send_data result, filename: "#{@disaster.name}-#{@disaster.date_of_occurence}-#{DateTime.now}.xls", type: 'text/xls; charset=utf-8' }
-            format.csv {send_data result, filename: "#{@disaster.name}-#{@disaster.date_of_occurence}-#{DateTime.now}.csv", type: 'text/csv; charset=utf-8' }
+            format.xls {send_data result, filename: "#{@disaster.name}-#{@disaster.date_of_occurence}-#{@evac_center.name}.xls", type: 'text/xls; charset=utf-8'}
+            format.csv {send_data result, filename: "#{@disaster.name}-#{@disaster.date_of_occurence}-#{@evac_center.name}.csv", type: 'text/csv; charset=utf-8'}
         end
     end
-    def generate_all
+    def generate_all # disasters/1/generate
         ## GRAND TOTAL OF ALL EVAC CENTERS
         @infantM = 0
         @toddlerM = 0
@@ -61,16 +69,40 @@ class GenerateReportController < ApplicationController
             @seniorF = @seniorF + helpers.getSeniors(center.id, @disaster.id, "Female")
         
         end
+        result = CSV.generate do |csv|
+            csv << ["GENERATED AS OF", Time.current]
+            csv << ["EVACUATION REPORT OF NAGA CITY"]
+            csv << ["DISASTER NAME", @disaster.name, "DISASTER TYPE",@disaster.disaster_type,"DATE OF OCCURENCE", @disaster.date_of_occurence]
+            csv << ["BARANGAY", "EVACUATION CENTER"].concat(age_group)
+            barangay_group.each do |brgy|
+                csv << [brgy, EvacCenter.where(barangay: brgy).length]
+                @evac_centers.each do |ev|
+                    if ev.barangay == brgy
+                        csv << ["", ev.name]
+                    end
+                end
+            end
+        end
         respond_to do |format|
             format.html
-            format.csv 
+            format.xls {send_data result, filename: "#{@disaster.name}-#{@disaster.date_of_occurence}.xls", type: 'text/xls; charset=utf-8'}
+            format.csv {send_data result, filename: "#{@disaster.name}-#{@disaster.date_of_occurence}.csv", type: 'text/csv; charset=utf-8'}
         end
-
     end
     private
 
     def age_group 
         titles = ["INFANT", "TODDLERS", "Preschoolers", "Schoolage","Teenage", "Adult", "Senior Citizens"]
     end
-
+    def barangay_group
+        titles = [
+            "Abella", "Bagumbayan Norte", "Bagumbayan Sur", "Balatas", 
+            "Calauag", "Cararayan", "Carolina", "Concepcion Grande", 
+            "Concepcion Pequeña", "Dayangdang", "Del Rosario", "Dinaga", 
+            "Igualdad Interior","Lerma","Liboton", "Mabolo", "Pacol", 
+            "Panicuason", "Peñafrancia", "Sabang", "San Felipe", 
+            "San Francisco", "San Isidro", "Santa Cruz", "Tabuco", 
+            "Tinago", "Triangulo"
+        ]
+    end
 end
