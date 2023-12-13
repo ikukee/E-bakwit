3ecf1b8fb5eb449e7aebb0fd51b5e2705fc202ee
ikukee <rmpanis2@gmail.com>
2023-12-13 14:00:21 +0800

    Update generate_report_controller.rb

--- app/controllers/generate_report_controller.rb
+++ app/controllers/generate_report_controller.rb
@@ -146,8 +146,7 @@ class GenerateReportController < ApplicationController
         p.serialize "#{Rails.root}/tmp/generate.xlsx.axlsx"
         #.serialize "#{Rails.root}/app/views/generate_report/generate.xlsx.axlsx"
         send_file("#{Rails.root}/tmp/generate.xlsx", filename:"#{@disaster.name}-#{@disaster.date_of_occurence}-#{@evac_center.name}.xlsx", type: "application/xlsx",disposition: 'inline')
-         
-        @format.xlsx { render p, template: 'generate', filename: "generate.xlsx.axlsx", layout:false}
+
         #render file: "#{Rails.root}/tmp/generate.xlsx.axlsx", layout: true
         
 
